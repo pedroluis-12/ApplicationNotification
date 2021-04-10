682abd67ce4130133939dee4c45ab254ab74dd9e
pedroluis <pedroluismc.12@gmail.com>
2021-04-09 22:13:03 -0300

    Initial commit

---
+++ app/src/main/java/com/courses/applicationnotification/MyFirebaseInstanceIdService.kt
@@ -0,0 +1,14 @@
+package com.courses.applicationnotification
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.iid.FirebaseInstanceIdService
+
+class MyFirebaseInstanceIdService : FirebaseInstanceIdService() {
+    override fun onTokenRefresh() {
+        Log.i("**newTokenService", FirebaseInstanceId.getInstance().token.toString())
+    }
+}
